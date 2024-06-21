aee1b6d35760d202a3c88bd3a1bd2e351c500167
Anandu Sankar <ananduap74@gmail.com>
2024-06-21 16:30:55 +0530

    21/06/2024

---
+++ pre_type.js
@@ -0,0 +1,13 @@
+//premitige type number
+var value = 1;
+var total = ++value + value;
+console.log(total);
+//premitive type string
+var value2 = 'hellow typescrpt';
+var result = value2 + ', ' + 'my namme is anandu';
+console.log(result);
+//premitive type boolean
+var published = true;
+if (published) {
+    console.log(true);
+}
---
+++ pre_type.ts
@@ -0,0 +1,21 @@
+//premitige type number
+let value =1;
+
+const total = ++value + value
+
+console.log(total)
+
+
+//premitive type string
+const value2= 'hellow typescrpt'
+
+const result = value2 + ', '+ 'my namme is anandu'
+
+console.log(result)
+
+
+//premitive type boolean
+const published =true
+if(published){
+console.log(true)
+}
